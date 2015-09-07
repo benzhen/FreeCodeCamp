ce706f44d24582fdf8ef98e86b1a554c44486bfb
benzhen <benzhen325@gmail.com>
2015-09-07 17:21:06 -0400

    Added comments to files

--- Basic_Front_End_Development_Projects/Zipline_Build_a_Personal_Portfolio_Webpage/js/index.js
+++ Basic_Front_End_Development_Projects/Zipline_Build_a_Personal_Portfolio_Webpage/js/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+  //add classes from animate.css to pulse when hovered
   $('.popInAndOut').hover(function() {
     $(this).addClass('animated pulse infinite');
   },function() {
     $(this).removeClass('animated pulse infinite');
   });
+  //animate the buttons to be bigger when hovered
   $('.popOut').hover(function(){
     $(this).animate({height: '+=10%', width: '+=10%'},500);
   },function(){
--- Basic_Front_End_Development_Projects/Zipline_Build_a_Random_Quote_Machine/js/index.js
+++ Basic_Front_End_Development_Projects/Zipline_Build_a_Random_Quote_Machine/js/index.js
@@ -1,28 +1,29 @@
 $(document).ready(function() {
+  // add classes from animate.css to pulse when hovered
   $('#quoteButton').hover(function(){
     $(this).addClass('animated pulse infinite');
   }, function() {
     $(this).removeClass('animated pulse infinite');
   });
-  
+  // make request to Forismatic API by URL and capture it in JSON format
+  // parse the quoteText and quoteAuthor to their elements 
   $('#quoteButton').click(function() {
    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
-   .done(function(quoteInfo) {
-     $('#quote').html("<h2>" + quoteInfo.quoteText + "</h2>");
-     if(quoteInfo.quoteAuthor !== ""){
-       $('#author').html("<h3>- " + quoteInfo.quoteAuthor + "</h3>");
-     } else {
-       $('#author').html("<h3>- Unknown</h3>");
-     }
-   });
+     .done(function(quoteInfo) {
+       $('#quote').html("<h2>" + quoteInfo.quoteText + "</h2>");
+       if(quoteInfo.quoteAuthor !== ""){
+         $('#author').html("<h3>- " + quoteInfo.quoteAuthor + "</h3>");
+       } else {
+         $('#author').html("<h3>- Unknown</h3>");
+       }
+    });
   });
-  
+  // send the shorten URL of the site and the quote on the site to be on the tweet
   $('#tweetThis').click(function() {
-    var text = "";
-    text += $('#quote').text() + '\n' + $('#author').text() + '\n\nTweet your own quotes\n';
+    var text = $('#quote').text() + '\n' + $('#author').text() + '\n\nTweet your own quotes\n';
     tweet("http://bit.ly/1Uxj9bs", text);
   });
-  
+  // open up a new window to twitter with the site URL and the quote in the textbox of the tweet
   function tweet(url, text) {
             url = encodeURIComponent(url);
             text = encodeURIComponent(text);
